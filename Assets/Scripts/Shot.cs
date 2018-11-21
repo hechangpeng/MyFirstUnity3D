4d1d2d5f2ab91a0da588e81a9dde40f09456dad6
hechangpeng <571772435@qq.com>
2018-11-21 17:50:00 +0800

    提交代码

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+    public GameObject clickbox;
+    private int speed = 30;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            GameObject gameObject= GameObject.Instantiate(clickbox,this.transform.position,this.transform.rotation);
+            Rigidbody rgb = gameObject.GetComponent<Rigidbody>();
+            rgb.velocity = this.transform.forward * speed;
+        }
+	}
+}
